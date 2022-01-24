964819765fdfdbf8b1d6d009df77f97ec6d00764
ittimfn <ittimfn+github@gmail.com>
2022-01-24 13:38:44 +0900

    Dockerコンテナ外でもログ出力できるように修正

--- app/src/logutil.py
+++ app/src/logutil.py
@@ -10,4 +10,5 @@ class LogUtil:
     """
     with open(log_conf_path, mode='r') as f:
       log_conf = json.loads(f.read())
+      log_conf['handlers']['fileHandler']['filename'] = os.getenv('PYTHON_APP_HOME') + '/' + log_conf['handlers']['fileHandler']['filename']
     return log_conf
