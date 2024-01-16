55d7cbe537925b8a2e4a806e8c36d32ab0601a55
SampleUser0001 <ittimfn+github@gmail.com>
2024-01-17 00:23:57 +0900

    sh間のenvファイルのパス指定方法を統一

--- start_venv.sh
+++ start_venv.sh
@@ -4,13 +4,13 @@ source venv/bin/activate
 
 pushd app > /dev/null
 
-ln -s ./src/$1.env ./src/.env
+ln -s $(pwd)/src/$1.env $(pwd)/src/.env
 
 # 引数の数に応じて変更する
 # bash start.sh $1 $2 ...
 bash start.sh
 
-unlink ./src/.env
+unlink $(pwd)/src/.env
 
 popd > /dev/null
 
