e8c7c102dc649f1cbf23976fd09492d4336febfa
SampleUser0001 <ittimfn+github@gmail.com>
2024-01-16 23:01:31 +0900

    実行時に読み込むenvファイルを引数で指定できるように修正。 test_*.pyとそれ以外を分けてログファイル出力するように修正。

--- start_venv.sh
+++ start_venv.sh
@@ -3,9 +3,14 @@
 source venv/bin/activate
 
 pushd app > /dev/null
+
+ln -s ./src/$1.env ./src/.env
+
 # 引数の数に応じて変更する
 # bash start.sh $1 $2 ...
 bash start.sh
+
+unlink ./src/.env
 
 popd > /dev/null
 
