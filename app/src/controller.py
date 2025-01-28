d84606bc3e73d572ef37eaeb98357a1bac638df7
SampleUser0001 <ittimfn+github@gmail.com>
2025-01-28 09:46:00 +0900

    Feature/logging setting sample (#15)
    * INFO出力用メソッドとDEBUG出力用メソッドを分ける
    
    * メソッド単位でロガーを設定できるように修正
    
    * アノテーション用メソッドの共通化

--- app/src/controller.py
+++ app/src/controller.py
@@ -3,8 +3,7 @@ from logging import getLogger, config, DEBUG, NOTSET
 import os
 
 # import sys
-from logutil import LogUtil
-from functools import wraps
+from logutil import LogUtil, apply_logger
 
 PYTHON_APP_HOME = os.getenv('PYTHON_APP_HOME')
 LOG_CONFIG_FILE = ['config', 'log_config.json']
@@ -14,14 +13,6 @@ log_conf = LogUtil.get_log_conf(os.path.join(PYTHON_APP_HOME, *LOG_CONFIG_FILE))
 config.dictConfig(log_conf)
 logger.setLevel(DEBUG)
 logger.propagate = False
-
-def apply_logger(cls):
-    for attr_name, attr_value in cls.__dict__.items():
-        if callable(attr_value):  # メソッドかどうか確認
-            logger_name = f"{__name__}.{cls.__name__}.{attr_name}"
-            decorated = LogUtil.dynamic_logger(logger_name)(attr_value)
-            setattr(cls, attr_name, decorated)
-    return cls
 
 @apply_logger
 class SampleController():
