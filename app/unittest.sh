8f12f97b0c15b561e3dc3758deab0ad340d6cf55
SampleUser0001 <ittimfn+github@gmail.com>
2025-02-07 19:16:24 +0900

    テスト時に対象のクラス、関数を指定できるようにする

--- app/unittest.sh
+++ app/unittest.sh
@@ -3,10 +3,16 @@
 source common.sh
 
 pushd src > /dev/null
-for d in `find . -type d ` ; do
-    echo ${d}
-    python -m unittest discover -v ${d}
-done 
+
+if [ $# -eq 0 ]; then
+    for d in `find . -type d ` ; do
+        echo ${d}
+        python -m unittest discover -v ${d}
+    done 
+else
+    python -m unittest $1
+fi
+
 
 popd
 
--- start_test.sh
+++ start_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# source venv/bin/activate
+source venv/bin/activate
 
 pushd app > /dev/null
 
@@ -8,10 +8,10 @@ ln -s $(pwd)/src/$1.json $(pwd)/src/environment.json
 
 # 引数の数に応じて変更する
 # bash start.sh $1 $2 ...
-bash unittest.sh
+bash unittest.sh $2
 
 unlink $(pwd)/src/environment.json
 
 popd > /dev/null
 
-#deactivate
+deactivate
